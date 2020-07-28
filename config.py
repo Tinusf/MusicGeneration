037f795432aa73191df2a1caa4afdd23f25788ff
Tinus Flagstad <t-tiflag@microsoft.com>
2020-07-28 15:14:42 +0200

    First attempt at creating piano and violin music.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # How many epochs should be trained on.
-EPOCHS = 10
+EPOCHS = 3
 # Which model should be used. Options: "LSTM", "WaveNet"
 WHICH_MODEL = "LSTM"
 # Should you load the weights from the model.
@@ -7,9 +7,13 @@ SHOULD_LOAD_MODEL = True
 # Where the models are stored.
 MODEL_DIRECTORY = "saved_models/lstm/"
 # Which model within the MODEL_DIRECTORY should be used for loading and saving the model.
-MODEL_FILE = "model.h5"
+MODEL_FILE = "model7.h5"
+# Which directory the midi files are located in.
+MIDI_DIRECTORY = "data/pianoviolin/"
+# Should you just load the pickle files instead of parsing all the midi files.
+LOAD_CACHED_MIDI_FILES = False
 # Filter out infrequently used notes.
-FILTER_INFREQUENTLY = True
+FILTER_INFREQUENTLY = False
 # Use one hot encoding.
 ONE_HOT = True
 # When generating new music should you just use the greedy best choice, or should you use probabilities
